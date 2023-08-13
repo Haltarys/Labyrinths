572d68073bc5731695e065c769306d689aad8b86
Haltarys <45515869+Haltarys@users.noreply.github.com>
2023-08-13 20:36:46 +0200

    WIP: feat(labyrinths): add basic ruby gem

---
+++ lib/labyrinths.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "labyrinths/version"
+
+module Labyrinths
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/labyrinths_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Labyrinths do
+  it "has a version number" do
+    expect(Labyrinths::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
