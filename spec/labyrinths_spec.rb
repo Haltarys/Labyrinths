3869764d7a9f7c2b13602290917080346fda53c5
Haltarys <45515869+Haltarys@users.noreply.github.com>
2023-09-03 19:39:15 +0200

    test: remove placeholder test

--- spec/labyrinths_spec.rb
+++ spec/labyrinths_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Labyrinths do
   it "has a version number" do
     expect(Labyrinths::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
